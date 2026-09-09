9d7ecf909cdcc9654d58c0c54159dc652d3dcb93
Fran <140926702+FranTheCaveman@users.noreply.github.com>
2026-09-10 06:17:27 +1200

    navigation fix for github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     site: 'https://franthecaveman.github.io',
     base: '/portfolio', 
+    build: {
+        assets: 'assets',
+    },
 });
