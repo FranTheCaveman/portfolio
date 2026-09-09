d18ac08b6b6dc5a9c51da6b4d09db45fa800e036
Fran <140926702+FranTheCaveman@users.noreply.github.com>
2026-09-10 05:53:16 +1200

    Completed projects listing (todo: detail page), added temp solitaire page, added contact links in home

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,4 @@
 import { defineConfig } from 'astro/config';
-import remarkGfm from 'remark-gfm';
-import rehypeExternalLinks from 'rehype-external-links';
 
 export default defineConfig({
-  markdown: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [
-      [rehypeExternalLinks, { target: '_blank', rel: ['nofollow'] }]
-    ],
-  },
 });
