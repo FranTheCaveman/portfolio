061d630453c4ca13c048e98745fb831267831807
Fran <140926702+FranTheCaveman@users.noreply.github.com>
2026-09-10 06:26:01 +1200

    fix routing agan again

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
     site: 'https://franthecaveman.github.io/portfolio/',
+    base: '/portfolio/', 
     build: {
         assets: 'assets',
     },
